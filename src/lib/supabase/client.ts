337f34df597c107f96cf750527abc297c2ad4801
Angel Contreras Garcia <146035436+angelcgar@users.noreply.github.com>
2025-09-20 19:00:52 -0600

    feat: add Supabase client configuration for browser and server environments

---
+++ src/lib/supabase/client.ts
@@ -0,0 +1,9 @@
+import { createBrowserClient } from '@supabase/ssr';
+
+import { envs } from '@/config/envs';
+
+// Cliente para el navegador (React components "use client")
+export const createClient = createBrowserClient(
+	envs.NEXT_PUBLIC_SUPABASE_URL,
+	envs.NEXT_PUBLIC_SUPABASE_ANON_KEY,
+);
---
+++ src/lib/supabase/server.ts
@@ -0,0 +1,33 @@
+import { cookies } from 'next/headers';
+
+import { createServerClient } from '@supabase/ssr';
+
+import { envs } from '@/config/envs';
+
+// Cliente para el servidor (layouts, RSC, server actions, etc.)
+export async function createClient() {
+	const cookieStore = await cookies();
+
+	return createServerClient(
+		envs.NEXT_PUBLIC_SUPABASE_URL,
+		envs.NEXT_PUBLIC_SUPABASE_ANON_KEY,
+		{
+			cookies: {
+				getAll() {
+					return cookieStore.getAll();
+				},
+				setAll(cookiesToSet) {
+					try {
+						cookiesToSet.forEach(({ name, value, options }) => {
+							cookieStore.set(name, value, options);
+						});
+					} catch {
+						// The `setAll` method was called from a Server Component.
+						// This can be ignored if you have middleware refreshing
+						// user sessions.
+					}
+				},
+			},
+		},
+	);
+}
