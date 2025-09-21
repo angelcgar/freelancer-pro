fdba8fceb51c489d7e3830e87c68399ce71ce0eb
Angel Contreras Garcia <146035436+angelcgar@users.noreply.github.com>
2025-09-20 18:57:30 -0600

    refactor: using enviroment variables correctly for NextJs

--- src/config/envs.ts
+++ src/config/envs.ts
@@ -1,32 +1,30 @@
-import { get } from 'env-var';
+import { createEnv } from '@t3-oss/env-nextjs';
+import { z } from 'zod';
 
-export const env = {
-	NEXT_PUBLIC_CLERK_PUBLISHABLE_KEY: get('NEXT_PUBLIC_CLERK_PUBLISHABLE_KEY')
-		.required()
-		.asString(),
-	CLERK_SECRET_KEY: get('CLERK_SECRET_KEY').required().asString(),
+export const envs = createEnv({
+	/*
+	 * Serverside Environment variables, not available on the client.
+	 * Will throw if you access these variables on the client.
+	 */
+	server: {
+		CLERK_SECRET_KEY: z.string(),
+	},
 
-	// Supabase
-	NEXT_PUBLIC_SUPABASE_URL: get('NEXT_PUBLIC_SUPABASE_URL')
-		.required()
-		.asString(),
-	NEXT_PUBLIC_SUPABASE_ANON_KEY: get('NEXT_PUBLIC_SUPABASE_ANON_KEY')
-		.required()
-		.asString(),
-	SUPABASE_SERVICE_ROLE_KEY: get('SUPABASE_SERVICE_ROLE_KEY')
-		.required()
-		.asString(),
+	/*
+	 * Environment variables available on the client (and server).
+	 *
+	 * You'll get type errors if these are not prefixed with NEXT_PUBLIC_.
+	 */
+	client: {
+		NEXT_PUBLIC_SUPABASE_URL: z.string(),
+		NEXT_PUBLIC_SUPABASE_ANON_KEY: z.string(),
+	},
 
-	// Stripe
-	NEXT_PUBLIC_STRIPE_PUBLISHABLE_KEY: get('NEXT_PUBLIC_STRIPE_PUBLISHABLE_KEY')
-		.required()
-		.asString(),
-	STRIPE_SECRET_KEY: get('STRIPE_SECRET_KEY').required().asString(),
-	STRIPE_WEBHOOK_SECRET: get('STRIPE_WEBHOOK_SECRET').required().asString(),
-
-	// Resend
-	RESEND_API_KEY: get('RESEND_API_KEY').required().asString(),
-
-	// App
-	NEXT_PUBLIC_APP_URL: get('NEXT_PUBLIC_APP_URL').asString(),
-};
+	runtimeEnv: {
+		// server
+		CLERK_SECRET_KEY: process.env.CLERK_SECRET_KEY,
+		// client
+		NEXT_PUBLIC_SUPABASE_URL: process.env.NEXT_PUBLIC_SUPABASE_URL,
+		NEXT_PUBLIC_SUPABASE_ANON_KEY: process.env.NEXT_PUBLIC_SUPABASE_ANON_KEY,
+	},
+});
