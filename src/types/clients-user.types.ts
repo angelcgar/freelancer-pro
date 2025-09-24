8fbf8b6cf2fe0f86024729b98725c38c0a80eef3
Angel Contreras Garcia <146035436+angelcgar@users.noreply.github.com>
2025-09-24 11:35:41 -0600

    feat: add ClientUser interface for managing client information

---
+++ src/types/clients-user.types.ts
@@ -0,0 +1,12 @@
+export interface ClientUser {
+  id:         string;
+  user_id:    string;
+  name:       string;
+  email:      string;
+  phone:      string;
+  company:    string;
+  address:    string;
+  notes:      string;
+  created_at: Date;
+  updated_at: string;
+}
--- src/types/index.ts
+++ src/types/index.ts
@@ -1,2 +1,3 @@
 export * from './auth-user.types';
 export * from './profile-user.types';
+export * from './clients-user.types';
