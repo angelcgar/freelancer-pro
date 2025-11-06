dae53aa2c03784f0ab85df33eef51b3181fee8d0
Angel Contreras Garcia <146035436+angelcgar@users.noreply.github.com>
2025-11-06 14:36:03 -0600

    feat: implementar la pagina profile y mostrar la información del usuario en pantalla

--- src/app/(freelancer)/dashboard/profile/page.tsx
+++ src/app/(freelancer)/dashboard/profile/page.tsx
@@ -1,9 +1,23 @@
+'use client';
+
+import { useCurrentProfile } from '@/hooks/useCurrentProfile';
+
 export default function ProfilePage() {
-	// todo: Get user
+	const { user, profile, loading } = useCurrentProfile();
+
+	if (loading) return <p>Loading...</p>;
 
 	return (
 		<div>
 			<h1>Profile page</h1>
+			<h2>id</h2>
+			<pre>{user?.app_metadata.provider}</pre>
+			<h2>email</h2>
+			<pre>{user?.email}</pre>
+
+			<pre>{JSON.stringify(user, null, 2)}</pre>
+			<h2>Profile current user</h2>
+			<pre>{JSON.stringify(profile, null, 2)}</pre>
 		</div>
 	);
 }
